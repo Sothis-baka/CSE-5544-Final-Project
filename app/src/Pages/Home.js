09f4c18dc0b72795c4cab082a2b0d6dfd71f3606
Cyrus Li <70495046+Sothis-baka@users.noreply.github.com>
2021-04-06 00:20:51 -0500

    home page complete

--- app/src/Pages/Home.js
+++ app/src/Pages/Home.js
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+
+import '../Styles/Home.css';
+import '../Styles/button.css';
+
 const Home = () => {
     return(
-        <div>Home</div>
+        <div id='HomeWrapper'>
+            <h1 id='homeTitle'>CSE 5544 Final Project</h1>
+            <p id='Author'>Adithya Reji, Cyrus Li, Taoqi Yang</p>
+            <div className='BtnGroup'>
+                <Link to='/visual1'><button className='btn-1'><span>Visualization 1</span></button></Link>
+                <Link to='/visual2'><button className='btn-3'><span>Visualization 2</span></button></Link>
+                <Link to='/visual3'><button className='btn-4'><span>Visualization 3</span></button></Link>
+            </div>
+        </div>
     );
-}
+};
 
 export default Home;
